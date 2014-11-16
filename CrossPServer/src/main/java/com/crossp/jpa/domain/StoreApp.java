52e638586fabfb65987aef7784322cc4f2c15684
Shev Yan <YanDong_8212@163.com>
2014-11-16 12:10:55 +0800

    .

--- CrossPServer/src/main/java/com/crossp/jpa/domain/StoreApp.java
+++ CrossPServer/src/main/java/com/crossp/jpa/domain/StoreApp.java
@@ -1,16 +1,9 @@
 package com.crossp.jpa.domain;
 
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +13,14 @@ public class StoreApp{
 	@Id
 	@GeneratedValue
 	private Long id;
+	
+	private String appstoreId;
+	private String name;
+	private String icon175Link;
+	private String icon350Link;
+	private String type;
+	private String description;
+	private String screenPictures;
 	
 	public Long getId() {
 		return id;
@@ -63,40 +64,12 @@ public class StoreApp{
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getScreenPicture1Link() {
-		return screenPicture1Link;
+	public String getScreenPictures() {
+		return screenPictures;
 	}
-	public void setScreenPicture1Link(String screenPicture1Link) {
-		this.screenPicture1Link = screenPicture1Link;
+	public void setScreenPictures(String screenPictures) {
+		this.screenPictures = screenPictures;
 	}
-	public String getScreenPicture2Link() {
-		return screenPicture2Link;
-	}
-	public void setScreenPicture2Link(String screenPicture2Link) {
-		this.screenPicture2Link = screenPicture2Link;
-	}
-	public String getScreenPicture3Link() {
-		return screenPicture3Link;
-	}
-	public void setScreenPicture3Link(String screenPicture3Link) {
-		this.screenPicture3Link = screenPicture3Link;
-	}
-	public String getScreenPicture4Link() {
-		return screenPicture4Link;
-	}
-	public void setScreenPicture4Link(String screenPicture4Link) {
-		this.screenPicture4Link = screenPicture4Link;
-	}
-	private String appstoreId;
-	private String name;
-	private String icon175Link;
-	private String icon350Link;
-	private String type;
-	private String description;
-	private String screenPicture1Link;
-	private String screenPicture2Link;
-	private String screenPicture3Link;
-	private String screenPicture4Link;
 	
 	
 }
